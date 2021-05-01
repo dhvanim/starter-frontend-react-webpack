4d8c38d0858c873be44786073254f0c4f433b8d9
dhvanim <61432412+dhvanim@users.noreply.github.com>
2021-04-30 20:47:35 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import '../public/style.css';
+
+const App = () => {
+  return (
+      <div>
+        <h3>Hello World :-)</h3>
+        <p>This is a sample starter!</p>
+      </div>
+  );
+};
+
+export default App;
---
+++ webpack.common.js
@@ -0,0 +1,61 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: {
+    app: './src/index.js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+        template: './public/index.html',
+        favicon: './favicon.ico',
+    }),
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  module: {
+    rules: [      
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-loader',
+          },
+        ],
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.(png|jpe?g|gif|ico|eot|ttf|woff)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+          },
+        ],
+      },
+      {
+        test: /\.mp4$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "[name].[ext]",
+              outputPath: "video"
+            }
+          }
+        ]
+      }
+    ]
+  },
+};
